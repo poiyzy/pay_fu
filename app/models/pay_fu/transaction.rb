aa7bfd0cb13cad04f4af5ba886d21da41ff3137e
Roy Young <zhuoyuyang@gmail.com>
2012-12-09 18:40:58 +0800

    Add update payment function

--- app/models/pay_fu/transaction.rb
+++ app/models/pay_fu/transaction.rb
@@ -1,5 +1,17 @@
 module PayFu
   class Transaction < ActiveRecord::Base
+    belongs_to :user, class_name: User
     attr_accessible :transaction_id, :transaction_type, :payment_status, :payment_date, :gross, :raw_post, :type, :user_id
+
+    after_create :update_product_to_user
+    after_save :update_product_to_user
+
+    def update_product_to_user
+      if self.payment_status == "TRADE_FINISHED"
+        self.user.payment.created_at = self.updated_at
+        self.user.payment.save
+      end
+    end
   end
 end
+
